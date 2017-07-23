9caf9fc445ecfa83ee92b99b093b9c88d5a7c70f
oldmansulaiman <sulaiman2199.saq@gmail.com>
2017-07-23 15:39:22 +0530

    test Comit

--- app/src/main/java/com/androidtutorialpoint/cardviewtutorial/MainActivity.java
+++ app/src/main/java/com/androidtutorialpoint/cardviewtutorial/MainActivity.java
@@ -4,8 +4,15 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button signOutButton;
+    private FirebaseAuth auth;
 
 
     @Override
@@ -13,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        auth = FirebaseAuth.getInstance();
+        signOutButton = (Button) findViewById(R.id.sign_out);
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
 
@@ -26,7 +36,19 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
-
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOutButton();
+            }
+        });
 
     }
+
+    //sign out method
+    public void signOutButton() {
+        auth.signOut();
+    }
+
+
 }
