513b0d2939450a56a9755111908725c4504c42e7
oldmansulaiman <sulaiman2199.saq@gmail.com>
2017-07-23 12:40:28 +0530

    test Comit

--- app/src/main/java/com/androidtutorialpoint/cardviewtutorial/AttendanceActivities/Civil/Civil_Att_First_A_Activity.java
+++ app/src/main/java/com/androidtutorialpoint/cardviewtutorial/AttendanceActivities/Civil/Civil_Att_First_A_Activity.java
@@ -12,28 +12,12 @@ import com.google.firebase.database.ValueEventListener;
 
 public class Civil_Att_First_A_Activity extends AppCompatActivity {
 
-    TextView txtValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_civil__att__first__a_);
 
-        final
-        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-        firebaseDatabase.getReference("Name").child("student").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                TextView txtValue = (TextView) findViewById(R.id.civil_att_first_a_text1);
 
-                String value = dataSnapshot.getValue(String.class);
-                txtValue.setText(value);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
     }
 }
